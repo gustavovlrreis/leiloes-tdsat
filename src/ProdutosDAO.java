72f3196b7c0bb74be9a621e79fc615c0a95f1afb
gustavovlrreis <138333592+gustavovlrreis@users.noreply.github.com>
2023-07-09 17:29:55 -0300

    botão de salvar cadastrando produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -22,12 +23,27 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public boolean cadastrarProduto (ProdutosDTO produto){
         
         
-        //conn = new conectaDAO().connectDB();
+        conn = new conectaDAO().connectDB();
         
-        
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) "
+                    + "VALUES(?,?,?)");
+            
+            prep.setString(1,produto.getNome());
+            prep.setInt(2,produto.getValor());
+            prep.setString(3,produto.getStatus());
+            
+            prep.executeUpdate();
+            
+            return true;
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            
+            return false;
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
